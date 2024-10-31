4d965f229d3655fff2968df1bf6cddc2dc36d63f
xjtu-men <136139347+xjtu-men@users.noreply.github.com>
2024-10-31 10:50:18 +0800

    add user fields

--- current_user_provider.rb
+++ current_user_provider.rb
@@ -7,7 +7,11 @@ class ExCurrentUserProvider < Auth::DefaultCurrentUserProvider
     require 'openssl' if !defined?(OpenSSL)
     require 'base64' if !defined?(Base64)
 
-    payload = { username: user.username, user_id: user.id, avatar: user.avatar_template, group: user.title }
+    payload = {  id: user.id, username: user.username, name: user.name,
+     admin:user.admin, moderator:user.moderator, trust_level: user.trust_level,
+     avatar_template: user.avatar_template, title: user.title,
+     groups: user.groups, locale: user.locale,
+     silenced_till: user.silenced_till , staged: user.staged, active: user.active}
     payload_sha = Digest::SHA256.hexdigest payload.to_json
     hash_function = OpenSSL::Digest.new('sha256')
     hmac = OpenSSL::HMAC.hexdigest(hash_function, SiteSetting.cookie_ui_key, payload_sha)
